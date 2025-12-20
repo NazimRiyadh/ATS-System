827010b161476e757a0af8e2386b38d1ae2010ac
NazimRiyadh <neariyadh786@gmail.com>
2025-12-20 18:23:22 +0600

    ready to pull

--- Final Version/inspect_lightrag.py
+++
@@ -1,8 +0,0 @@
-
-import inspect
-from lightrag import LightRAG
-
-print("LightRAG init parameters:")
-sig = inspect.signature(LightRAG.__init__)
-for name, param in sig.parameters.items():
-    print(f"- {name}")
